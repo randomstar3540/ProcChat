7d4dfcd3eaaff7fec88200617fd1d7a7523eae03
randomstar3540 <randomstar3540@gmail.com>
2021-05-08 15:08:07 +1000

    1

--- server.c
+++ server.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "server.h"
 
 int main(int argc, char** argv) {
     // Your code here
-    printf("test\n");
+    int pipe[2];
+    char * rd_pipe = "_RD";
+    char * wr_pipe = "_WR";
+
+    mkfifo(rd_pipe, 666);
+    mkfifo(wr_pipe, 666);
+
+    pipe[0] = open(rd_pipe, O_RDONLY);
+    pipe[1] = open(rd_pipe, O_WRONLY);
+
     return 0;
 }
