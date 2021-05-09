c8ecc2a605fd55e42e07edc1a4d691749eeb63ed
randomstar3540 <randomstar3540@gmail.com>
2021-05-09 15:27:32 +1000

    1

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "server.h"
 
@@ -18,12 +19,23 @@ int main(int argc, char** argv) {
     char p_RD_name[515];
     char p_WR_name[515];
     char domain_path[257];
+    pid_t child;
+    while(1){
+        p = open(gevent, O_RDONLY);
+        read(p,message,2048);
+        close(p);
 
-    struct stat st = {0};
-
-    p = open(gevent, O_RDONLY);
-    read(p,message,2048);
-    close(p);
+        child = fork();
+        if (child == -1) {
+            printf("fork failed\n");
+            exit(-1);
+        }
+        if (child) {
+            printf("connect!\n"); //parent
+        } else {
+            break;//child
+        }
+    }
 
     printf("%s",message);
     // uint16_t tcode = message[0] << 8 | message[1];
@@ -33,14 +45,14 @@ int main(int argc, char** argv) {
     sprintf(p_RD_name,"%s/%s_RD",domain,id);
     sprintf(p_WR_name,"%s/%s_WR",domain,id);
     sprintf(domain_path,"%s/",domain);
+
+    struct stat st = {0};
     if (stat(domain_path, &st) == -1) {
         mkdir(domain_path, 0700);
     }
     mkfifo(p_RD_name, 0666);
     mkfifo(p_WR_name, 0666);
     sleep(5);
-
-    unlink(gevent);
     unlink(p_RD_name);
     unlink(p_WR_name);
     remove(domain_path);
