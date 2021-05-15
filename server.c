f786fcafd30e9c47ae20644c0682ea32b1b5863a
randomstar3540 <randomstar3540@gmail.com>
2021-05-15 19:06:12 +1000

    1

--- server.c
+++ server.c
@@ -14,16 +14,18 @@ int main(int argc, char** argv) {
     char * gevent = "gevent";
     mkfifo(gevent, 0666);
     char message[2048];
-    char id[256];
-    char domain[256];
-    char p_RD_name[515];
-    char p_WR_name[515];
-    char domain_path[257];
+
     pid_t child;
     while(1){
         p = open(gevent, O_RDONLY);
         read(p,message,2048);
         close(p);
+
+        uint16_t tcode = message[0] << 8 | message[1];
+
+        if(tcode != 0){
+            continue;
+        }
 
         child = fork();
         if (child == -1) {
@@ -31,18 +33,23 @@ int main(int argc, char** argv) {
             exit(-1);
         }
         if (child) {
-            printf("connect!\n"); //parent
-        } else {
+            printf("%d\n",child);
+        }else{
             break;//child
         }
     }
 
-    // uint16_t tcode = message[0] << 8 | message[1];
+    char id[256];
+    char domain[256];
     memcpy(id,&message[2],256);
     memcpy(domain,&message[2+256],256);
 
+    char p_RD_name[515];
+    char p_WR_name[515];
     sprintf(p_RD_name,"%s/%s_RD",domain,id);
     sprintf(p_WR_name,"%s/%s_WR",domain,id);
+
+    char domain_path[257];
     sprintf(domain_path,"%s/",domain);
 
     struct stat st = {0};
@@ -51,6 +58,25 @@ int main(int argc, char** argv) {
     }
     mkfifo(p_RD_name, 0666);
     mkfifo(p_WR_name, 0666);
+
+    while(1){
+        p = open(p_WR_name, O_RDONLY);
+        read(p,message,2048);
+        close(p);
+
+        uint16_t tcode = message[0] << 8 | message[1];
+
+        if(tcode == 1){
+            continue;
+        }
+        else if(tcode == 7){
+            break;
+        }
+    }
+    printf("disconnect!\n");
+
+    unlink(p_RD_name);
+    unlink(p_WR_name);
 
     return 0;
 }
