5e1c6544e56479094b0e82edb2a35b766858500c
randomstar3540 <randomstar3540@gmail.com>
2021-05-15 21:21:46 +1000

    1

--- server.c
+++ server.c
@@ -6,24 +6,75 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 #include "server.h"
+
+void say_handler(char *domain, char* self, char* message){
+    DIR *dp;
+    struct dirent *file;
+    struct stat file_stat;
+    int p;
+    char self_pipe_name[PIPE_NAME_MAX];
+    char response[MESSAGE_LEN];
+
+    sprintf(self_pipe_name,"%s_RD",self);
+
+    if((dp = opendir(domain)) == NULL) {
+        fprintf(stderr,"cannot open directory: %s\n", domain);
+        return;
+    }
+
+    chdir(domain);
+
+    while((file = readdir(dp)) != NULL) {
+
+        stat(file->d_name,&file_stat);
+        if(!S_ISFIFO(file_stat.st_mode)) {
+            continue;
+        }
+        if(strcmp(&((file->d_name)[5]), "_RD") != 0){
+            continue;
+        }
+
+        printf("%s\n",file->d_name);
+
+        if(strcmp(file->d_name, self_pipe_name) == 0){
+            continue;
+        }
+
+        memset(response,0,2048);
+        response[1] = RECEIVE;
+        strcpy(&response[TYPE_LEN],self);
+        memcpy(&response[TYPE_LEN+PIPE_NAME_MAX],
+               &message[TYPE_LEN], 1790);
+
+        p = open(file->d_name, O_WRONLY);
+        write(p,response,2048);
+        close(p);
+
+    }
+
+    chdir("..");
+    closedir(dp);
+}
+
 
 int main(int argc, char** argv) {
     int p;
     char * gevent = "gevent";
     mkfifo(gevent, 0666);
-    char message[2048];
+    char message[MESSAGE_LEN];
 
     pid_t child;
     while(1){
         p = open(gevent, O_RDONLY);
-        read(p,message,2048);
+        read(p,message,MESSAGE_LEN);
         close(p);
 
         uint16_t tcode = message[0] << 8 | message[1];
 
-        if(tcode != 0){
+        if(tcode != CONNECT){
             continue;
         }
 
@@ -39,17 +90,17 @@ int main(int argc, char** argv) {
         }
     }
 
-    char id[256];
-    char domain[256];
-    memcpy(id,&message[2],256);
-    memcpy(domain,&message[2+256],256);
+    char id[PIPE_NAME_MAX];
+    char domain[PIPE_NAME_MAX];
+    memcpy(id,&message[2],PIPE_NAME_MAX);
+    memcpy(domain,&message[2+PIPE_NAME_MAX],PIPE_NAME_MAX);
 
-    char p_RD_name[515];
-    char p_WR_name[515];
+    char p_RD_name[PIPE_PATH_MAX];
+    char p_WR_name[PIPE_PATH_MAX];
     sprintf(p_RD_name,"%s/%s_RD",domain,id);
     sprintf(p_WR_name,"%s/%s_WR",domain,id);
 
-    char domain_path[257];
+    char domain_path[PIPE_NAME_MAX + 1];
     sprintf(domain_path,"%s/",domain);
 
     struct stat st = {0};
@@ -66,11 +117,23 @@ int main(int argc, char** argv) {
 
         uint16_t tcode = message[0] << 8 | message[1];
 
-        if(tcode == 1){
+        if(tcode == CONNECT){
             continue;
         }
-        else if(tcode == 7){
+        else if(tcode == SAY){
+            say_handler(domain,id,message);
+        }
+        else if(tcode == SAYCONT){
+            continue;
+        }
+        else if(tcode == PING){
+            continue;
+        }
+        else if(tcode == DISCONNECT){
             break;
+        }
+        else{
+            continue;
         }
     }
     printf("disconnect!\n");
--- server.h
+++ server.h
@@ -1,4 +1,20 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define MESSAGE_LEN 2048
+#define CONTENTS_LEN 2046
+#define TYPE_LEN 2
+#define SAY_MSG_LEN 1790
+#define PIPE_NAME_MAX 256
+#define PIPE_PATH_MAX 515
+
+#define CONNECT 0
+#define SAY 1
+#define SAYCONT 2
+#define RECEIVE 3
+#define RECVCONT 4
+#define PING 5
+#define PONG 6
+#define DISCONNECT 7
+
 #endif
